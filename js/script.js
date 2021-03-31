7b5fde8a1d9149c4c91173a85bb0a229442226ab
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-03-31 20:01:40 +0300

    header & finances adaptive

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-// let slides = document.querySelectorAll('.slides .slide');
-// let currentSlide = 0;
-// let slideInterval = setInterval(nextSlide,6000);
+let slides = document.querySelectorAll('.slides .slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 6000);
 
-// function nextSlide(){
-//   slides[currentSlide].className = 'slide';
-//   currentSlide = (currentSlide+1)%slides.length;
-//   slides[currentSlide].className = 'slide active';
-// }
+function nextSlide(){
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide+1) % slides.length;
+  slides[currentSlide].className = 'slide active';
+}
+
+for (let i = 0; i < slides.length; i++) {
+  slides[i].addEventListener('click', function(e) {
+    if(e.target.classList.contains('request-btn') || e.target.classList.contains('rate-btn')) {
+      return false;
+    } else {
+      slides[currentSlide].className = 'slide';
+      currentSlide = (currentSlide+1)%slides.length;
+      slides[currentSlide].className = 'slide active';
+    }
+  });
+}
